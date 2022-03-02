b9421ecc5a3f25768dd9f67ea386243803e6952a
Chris Harrison <cah@stpaulsschool.org.uk>
2022-03-02 09:01:48 +0000

    updated linked list

--- LinkedListExample/LinkedListExample.Lib/IntegerLinkedList.cs
+++ LinkedListExample/LinkedListExample.Lib/IntegerLinkedList.cs
@@ -8,19 +8,62 @@ namespace LinkedListExample.Lib
 {
     public class IntegerLinkedList
     {
-        private int v;
+        IntegerNode _head;
+
+        public IntegerLinkedList()
+        {
+            _head = null;
+        }
 
         public IntegerLinkedList(int v)
         {
-            this.v = v;
+            _head = new IntegerNode(v);
         }
 
-        public int Count { get; }
-        public int Sum { get;  }
+        public int Count => _head == null ? 0 : _head.Count;
+        public int Sum => _head == null ? 0 : _head.Sum;
 
         public void Append(int v)
         {
-            throw new NotImplementedException();
+            if (_head == null)
+                _head = new IntegerNode(v);
+            else
+                _head.Append(v);
+
+        }
+
+        public override string ToString()
+        {
+            return _head == null ? "{}" : $"{{{_head}}}";
+        }
+    }
+    class IntegerNode
+    {
+        int _value;
+        IntegerNode _next;
+
+        internal int Count => _next == null ? 1 : 1 + _next.Count;
+            
+        internal int Sum => _next == null ? _value : _value + _next.Sum;
+
+
+        internal IntegerNode(int v)
+        {
+            _value = v;
+            _next = null;
+        }
+
+        internal void Append(int v)
+        {
+            if (_next == null)
+                _next = new IntegerNode(v);
+            else
+                _next.Append(v);
+        }
+
+        public override string ToString()
+        {
+            return _next == null ? _value.ToString() : $"{_value}, {_next}";
         }
     }
 }
