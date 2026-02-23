5f39d93da39a6ec351e08389f5b4ca1c798a4880
spscah <cah@stpaulsschool.org.uk>
2026-02-23 17:21:11 +0000

    .net core version of the example classes.

---
+++ LinkedListIntroduction/LinkedListIntroduction.Lib/IntegerLinkedList.cs
@@ -0,0 +1,66 @@
+﻿namespace LinkedListIntroduction.Lib;
+
+public class IntegerLinkedList
+{
+    IntegerNode _head;
+    public IntegerLinkedList()
+    {
+        
+    }
+
+    public IntegerLinkedList(int v)
+    {
+        
+    }
+
+    public int Count { get { return 0; } }
+    public int Sum { get { return 0; } }
+
+    public void Append(int v)
+    {
+        
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+
+}
+
+public class IntegerNode
+{
+    int _value;
+    IntegerNode _next;
+
+    internal int Count
+    {
+        get
+        {
+            return 0;
+        }
+    }
+
+    internal int Sum
+    {
+        get
+        {
+            return 0;
+        }
+    }
+
+    internal IntegerNode(int v)
+    {
+        
+    }
+
+    internal void Append(int v)
+    {
+        
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
